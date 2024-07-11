605d5ec1e679e0c70ddebd8e8142341ea66a91c8
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-07-11 09:03:32 -0600

    added a button

--- src/main.jsx
+++ src/main.jsx
@@ -112,7 +112,7 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 8;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
@@ -120,7 +120,12 @@ function Footer() {
   // else alert("Sorry we're closed");
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open
+      {isOpen && (
+        <div className="order">
+          <p>WE're open until {closeHour}: 00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
